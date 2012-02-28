d89aecf67c3ca5f96661e801abe5f385e482d829
lianliuwei <lianliuwei@gmail.com>
2012-02-28 15:08:15 +0800

    create the clean sample for codejock test.

---
+++ resource.h
@@ -0,0 +1,45 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by RibbonMDISample.rc
+//
+#define IDR_MANIFEST                    1
+#define IDR_RIBBONTYPE_CNTR_IP          6
+#define IDR_LARGEICONS                  98
+#define IDR_SMALLICONS                  99
+#define IDP_OLE_INIT_FAILED             100
+#define IDP_FAILED_TO_CREATE            102
+#define IDR_MAINFRAME                   128
+#define IDR_RIBBONTYPE                  129
+#define ID_TAB_HOME                     130
+#define ID_GROUP_FILE                   131
+#define ID_TAB_VIEW                     132
+#define ID_TAB_INSERT                   133
+#define ID_TAB_EDIT                     133
+#define ID_GROUP_DOCUMENTVIEWS          134
+#define ID_GROUP_SHOWHIDE               135
+#define ID_GROUP_WINDOW                 136
+#define ID_GROUP_CLIPBOARD              137
+#define ID_FORMAT_PAINTER               138
+#define ID_GROUP_EDITING                139
+#define ID_EDIT_GOTO                    140
+#define ID_VIEW_NORMAL                  141
+#define ID_VIEW_FULLSCREEN              142
+#define ID_WINDOW_SWITCH                143
+#define ID_EDIT_SELECT                  144
+#define ID_CUSTOM_CONTROL               145
+#define IDB_GEAR                        300
+#define IDR_LAYOUT                      500
+#define ID_CANCEL_EDIT_CNTR             32768
+#define ID_VIEW_WORKSPACE               59394
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_3D_CONTROLS                     1
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
